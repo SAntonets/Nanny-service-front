c54e2b1dba3acfa56d7c1ab183695a6785a0e8bf
SAntonets <141963865+SAntonets@users.noreply.github.com>
2024-09-16 14:22:05 +0300

    add db api

--- src/firebse/firebase.js
+++ src/firebse/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs, addDoc } from 'firebase/firestore/lite';
+import { getFirestore } from 'firebase/firestore/lite';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,44 +19,3 @@ export const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 
-// Get a list of nannyes from your database
-async function addNanny(nanny) {
-  try {
-  const docRef = await addDoc(collection(db, "nannyes"), {
- name: nanny.name,
-    avatar_url: nanny.avatar_url,
-    birthday: nanny.birthday,
-    experience: nanny.experience,
-    reviews: nanny.reviews,
-    education: nanny.education,
-    kids_age: nanny.kids_age,
-    price_per_hour: nanny.price_per_hour,
-    location: nanny.location,
-    about: nanny.about,
-    characters: nanny.characters,
-    rating: nanny.rating,
-  });
-  console.log("Document written with ID: ", docRef.id);
-} catch (e) {
-  console.error("Error adding document: ", e);
-}
-}
-
-export function addNannyes(nannyesCollection) {
-  nannyesCollection.map(nanny => addNanny(nanny)
-  )
-}
-
-
-const nannyesCollectionRef = collection(db, "nannyes");
-
-export const nannnnnn = getDocs(nannyesCollectionRef)
-  .then((snapshot) => {
-    // Перегляньте дані
-    snapshot.docs.forEach((doc) => {
-      console.log(doc.id, " => ", doc.data());
-    });
-  })
-  .catch((error) => {
-    console.error("Помилка отримання даних:", error);
-  });
