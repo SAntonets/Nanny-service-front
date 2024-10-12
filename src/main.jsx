a14e5e3c5b68afe58adcc5105d06be1ebc19a20e
SAntonets <141963865+SAntonets@users.noreply.github.com>
2024-10-12 22:57:32 +0300

    add reducer

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,16 @@ import App from './App.jsx'
 import './index.css'
 import { StrictMode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 
 createRoot(document.getElementById('root')).render(
 
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider>
+      <BrowserRouter>
+       <App />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 )
