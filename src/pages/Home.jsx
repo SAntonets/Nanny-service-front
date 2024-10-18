9e84080cb8dab38ed9810211f30d4fd2016dd088
SAntonets <141963865+SAntonets@users.noreply.github.com>
2024-10-18 23:48:03 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AuthForm from '../components/AuthForm.jsx/AuthForm';
+import RegistrationForm from '../components/RegistrationForm/RegistrationForm';
 
 const Home = () => {
+  const [toSignIn, setToSignIn] = useState(true);
+    const toggleForm = () => {
+      if (toSignIn === true) {
+        setToSignIn(false);
+        return;
+      } else {
+        setToSignIn(true);
+        return;
+      }
+    }
   return (
-    <div><p>Home</p>
-    < AuthForm />
+    <div>
+      <p>Home</p>
+      <button onClick={toggleForm}>Toggle Auth Form</button>
+      <div>{toSignIn ? <AuthForm /> : <RegistrationForm/>} </div>
     </div>
-
   )
 }
 
